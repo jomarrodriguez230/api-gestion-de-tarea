f5ef2b5b99c47cb0cc50f339a181bf01504906f3
jomarrodriguez230 <est.jsrodriguezf0201@gmail.com>
2025-05-07 19:39:04 -0600

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,21 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use('/usuarios', require('./routes/usuarios'));
+app.use('/proyectos', require('./routes/proyectos'));
+app.use('/tareas', require('./routes/tareas'));
+app.use('/asignaciones', require('./routes/asignaciones'));
+app.use('/seguimiento', require('./routes/seguimiento'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
