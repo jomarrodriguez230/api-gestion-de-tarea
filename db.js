a5daf11ec4c448b8e41219a3dfa88b75f934c908
jomarrodriguez230 <136911731+jomarrodriguez230@users.noreply.github.com>
2025-05-13 21:13:01 -0600

    Update db.js

--- db.js
+++ db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
